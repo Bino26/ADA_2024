b9f7852f64f0e0a8b1571c0564c159ff8dffbd84
Bino26 <hbino1999@gmail.com>
2024-08-02 16:18:44 +0000

    🗂️ Add Menu

--- Module1/Exercice1/MonEtab/src/Main.java
+++ Module1/Exercice1/MonEtab/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+
+
+        System.out.println("******************************************************\n" +
+                "\n" +
+                "BIENVENU DANS L’APPLICATION ETAB\n" +
+                "\n" +
+                "******************************************************\n" +
+                "\n" +
+                "MENU:\n" +
+                "\n" +
+                "1: Ajouter un élève\n" +
+                "2: Supprimer un élève\n" +
+                "3: Modifier les informations de l'élève\n" +
+                "4: Lister les élèves\n" +
+                "5: Gérer les notes\n" +
+                "\n" +
+                "Date système : 12:30");
+
         Eleve junior = new Eleve(1,"Bino","Bino",24,"M");
         // Ajouter un eleve
 
